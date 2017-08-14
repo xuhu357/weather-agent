efdbe977f8dee33ac0dba87fb817b6f64e9b9067
xuhu357 <xuhu357@gmail.com>
2017-08-14 17:14:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# coding: utf8
 #!/usr/bin/env python
 
 from __future__ import print_function
@@ -10,6 +11,10 @@ from urllib.error import HTTPError
 
 import json
 import os
+import datetime
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 from flask import Flask
 from flask import request
@@ -17,7 +22,6 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -39,24 +43,53 @@ def processRequest(req):
     if req.get("result").get("action") != "weather":
         return {}
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
+    result = urlopen(yql_url).read()    
     data = json.loads(result)
-    res = makeWebhookResult(data)
+    
+    now = datetime.datetime.now()
+    now_tuple = now.timetuple()
+
+    now_str = ((now_tuple.tm_mday < 10) and (str(0) + str(now_tuple.tm_mday)) or (str(now_tuple.tm_mday)))+ " " + getMonthName(now_tuple.tm_mon) + " " + str(now_tuple.tm_year)
+    
+    day_str = getDateStrFromParameter(req)
+    
+    if now_str == day_str:
+        res = makeWebhookResult(data)
+    else:
+        res = makeWebhookForecastResult(data)   
+
     return res
 
 
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("sys_lc_wcity")
+    city = parameters.get("sys_lc_city")
     if city is None:
-        return None
+        city = parameters.get("sys_lc_wcity")
+        if(city is None):
+            return None
+    global global_city
+    global_city = city
+    
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    now = datetime.datetime.now()
+    now_tuple = now.timetuple()
+    #(a>b) and x or y
+    now_str = ((now_tuple.tm_mday < 10) and (str(0) + str(now_tuple.tm_mday)) or (str(now_tuple.tm_mday)))+ " " + getMonthName(now_tuple.tm_mon) + " " + str(now_tuple.tm_year)
+    day_str = getDateStrFromParameter(req)
+    
+    if now_str == day_str:
+        query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
+    else:
+        query = "select item.forecast, location from weather.forecast where woeid in (select woeid from geo.places(1) where text='"+ city +"') and u='c' and item.forecast.date='" + day_str +"'"
+    print(query);
+    return query
 
 
 def makeWebhookResult(data):
@@ -84,8 +117,7 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "오늘 " + global_city + " 날씨는 " + getKoreanWeatherCondition(condition.get('code')) + "이고, 기온은 " + condition.get('temp') + " " + units.get('temperature') + " 입니다."
 
     print("Response:")
     print(speech)
@@ -95,13 +127,169 @@ def makeWebhookResult(data):
         "displayText": speech,
         # "data": data,
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        "source": "apiai-weather-webhook-sample-customized"
+    }
+
+def makeWebhookForecastResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+    
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')   
+    if item is None:
+        return {}
+    
+    location = channel.get('location')
+    if location is None:
+        return {}
+    
+    forecast = item.get('forecast')
+    if forecast is None:
+        return {}
+    
+    date = forecast.get('date')
+    city = unicode(global_city)
+    ###str(date) + " in " + str(city) + " : " + str(forecast.get('text'))
+    speech = str(date_word) +" " + str(city) + u" 날씨는 " + getKoreanWeatherCondition(forecast.get('code')) + u"이고, 최고기온은 " + str(forecast.get('high')) + u" C , 최저기온은 " + str(forecast.get('low')) + u" C 입니다."
+    
+    print("Response:")
+    print(speech)
+    
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "source": "apiai-weather-webhook-sample-customized"
     }
 
 
+def getDateStrFromParameter(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    
+    global date_word
+    
+    day_word_map = {
+        u"오늘":0,
+        u"금일":0,
+        u"현재":0,
+        u"내일":1,
+        u"명일":1,
+        u"모레":2,
+        u"내일모레":2,
+        u"글피":3,
+        u"그글피":4,
+        u"그그글피":5        
+    }
+    
+    day = parameters.get("sys_dt_day")
+    if day is None:
+        now = datetime.datetime.now()
+        now_tuple = now.timetuple()
+        day = ((now_tuple.tm_mday < 10) and (str(0) + str(now_tuple.tm_mday)) or (str(now_tuple.tm_mday))) + " " + getMonthName(now_tuple.tm_mon) + " " + str(now_tuple.tm_year)
+        return day
+    
+    day = unicode(day)
+	
+	yy, mm, dd = day.split("-")
+        
+    day = dd + " " + getMonthName(int(mm)) + " " + str(yy)    
+             
+    return day
+
+def getMonthName(month):
+    month_map = {
+        1 :'Jan',
+        2 :'Feb',
+        3 :'Mar',
+        4 :'Apr',
+        5 :'May',
+        6 :'Jun',
+        7 :'Jul',
+        8 :'Aug',
+        9 :'Sep',
+        10:'Oct',
+        11:'Nov',
+        12:'Dec'       
+    }
+    return month_map[month]     
+
+def getKoreanWeatherCondition(weatherCondition):
+    korean_weather_map = {
+        "0":u"토네이도",
+        "1 Storm":u"열대성 폭풍",
+        "2":u"허리케인",
+        "3":u"뇌우 및 진눈깨비",
+        "4":u"뇌우",
+        "5":u"진눈깨비",
+        "6":u"진눈깨비",
+        "7":u"진눈깨비",
+        "8":u"진눈깨비",
+        "9":u"이슬비",
+        "10":u"비",
+        "11":u"소나기",
+        "12":u"소나기",
+        "13":u"눈",
+        "14":u"눈소나기",
+        "15":u"눈",
+        "16":u"눈",
+        "17":u"우박",
+        "18":u"진눈깨비",
+        "19":u"먼지많음",
+        "20":u"안개",
+        "21":u"옅은 안개",
+        "22":u"짙은 안개",
+        "23":u"강풍",
+        "24":u"바람이 많이 붐",
+        "25":u"추움",
+        "26":u"흐림",
+        "27":u"대체로 흐림(밤)",
+        "28":u"대체로 흐림(낮)",
+        "29":u"부분적 흐림(밤)",
+        "30":u"부분적 흐림(낮)",
+        "31":u"맑음(밤)",
+        "32":u"맑음(낮)",
+        "33":u"갬(밤)",
+        "34":u"갬(낮)",
+        "35":u"진눈깨비",
+        "36":u"더움",
+        "37":u"고립성 뇌우",
+        "38":u"산발성 뇌우",
+        "39":u"산발성 뇌우",
+        "40":u"산발성 소나기",
+        "41":u"폭설",
+        "42":u"부분적 눈소나기",
+        "43":u"폭설",
+        "44":u"부분적 흐림",
+        "45":u"소나기(뇌우)",
+        "46":u"눈소나기",
+        "47":u"고립성 소나기(뇌우)"
+    }
+    return korean_weather_map[weatherCondition]
+
+def getEnglishDateName(date_word):
+    day_map = {
+       u"오늘":"Today",
+        u"금일":"Today",
+        u"현재":"Now",
+        u"내일":"Tomorrow",
+        u"명일":"Tomorrow",
+        u"모레":"The day after tomorrow",
+        u"내일모레":"The day after tomorrow",
+        u"글피":"Two days after tommorrow",
+        u"그글피":"Three days after tomorrow",
+        u"그그글피":"Four days after tomorrow"    
+    }
+    return day_map[date_word]
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
     print("Starting app on port %d" % port)
-
     app.run(debug=False, port=port, host='0.0.0.0')
